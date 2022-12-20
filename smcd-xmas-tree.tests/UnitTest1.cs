f065c33c366968de5ddd4407a8aa9ea5366b6312
Stefan Boos <sboos@igus.net>
2022-12-20 17:33:13 +0100

    feat: tree of height 2
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Atsuhiro Endo <atcheri@gmail.com>

--- smcd-xmas-tree.tests/UnitTest1.cs
+++ smcd-xmas-tree.tests/UnitTest1.cs
@@ -7,12 +7,22 @@ public class UnitTest1
     {
         Assert.Equal("*",TreeGenerator.generate(1));
     }
+
+    [Fact]
+    public void treeOfheightTwo()
+    {
+        Assert.Equal(" * \n***",TreeGenerator.generate(2));
+    }
 }
 
 public class TreeGenerator
 {
     public static IEnumerable<char> generate(int i)
     {
-        return "*";
+        if (i == 1)
+        {
+            return "*";
+        }
+        return " * \n***";
     }
 }
