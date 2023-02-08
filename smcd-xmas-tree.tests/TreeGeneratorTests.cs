b6eb077789f7c60f9f0d54099ed5771a8dad5da3
Michael Mahlberg <mm@consulting-guild.de>
2023-02-08 16:56:14 +0100

    feat: Build the list for arbitrary tree height
    Co-authored-by: Stefan Boos <kontakt@boos.systems>

--- smcd-xmas-tree.tests/TreeGeneratorTests.cs
+++ smcd-xmas-tree.tests/TreeGeneratorTests.cs
@@ -26,6 +26,23 @@ public class TreeGeneratorTests
         Assert.Equal("",TreeGenerator.generate(0));
     }
 
+    [Fact]
+    public void StringArrayTreeHeight1()
+    {
+        Assert.Equal(new List<string> {"*"},TreeGenerator.GenerateRecursively(1));
+    }
+
+    [Fact]
+    public void StringArrayTreeHeight2()
+    {
+        Assert.Equal(new List<string> {"*", "***"},TreeGenerator.GenerateRecursively(2));
+    }
+
+    [Fact]
+    public void StringArrayTreeHeight4()
+    {
+        Assert.Equal(new List<string> {"*", "***","*****","*******"},TreeGenerator.GenerateRecursively(4));
+    }
 }
 
 public class TreeGenerator
@@ -34,9 +51,25 @@ public class TreeGenerator
     {
         return treeHeight switch { 
             0 => "", 
-            1 => "*",
+            1 => tempGenerator(treeHeight),
             _ => PadBothSides(generate(treeHeight - 1)) + "\n" + CreateLastTreeLine(treeHeight)
         };
+    }
+
+    public static string tempGenerator ( int treeHeight ) {
+        return string.Join ( "\n", GenerateRecursively(treeHeight));
+    }
+    
+    public static List<string> GenerateRecursively(int treeHeight)
+    {
+        if (treeHeight == 1)
+        {
+            return new List<string>{"*"};
+        }
+
+        var result = GenerateRecursively(treeHeight - 1);
+        result.Add(new string('*', 1+(treeHeight-1)*2));
+        return result;
     }
 
     private static string PadBothSides(string smallerTree)
