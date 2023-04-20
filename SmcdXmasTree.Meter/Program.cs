4d2eb26482a295e4d32b030f5ea5a88f40b60791
Bernhard Schenk <bernhardschenk@gmx.de>
2023-04-20 16:57:49 +0200

    feat: implemented first version of a meter
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Holger Janssen-Kroll <holger.janssen-kroll@aleri.de>
    Co-authored-by: Tugbay Atilla <tugbayatilla@gmail.com>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

---
+++ SmcdXmasTree.Meter/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Start the magic");
+HttpClient var = new HttpClient();
+
+// set timeout of 15 seconds
+var.Timeout = new TimeSpan(0,0,0,15);
+
+var res = await var.GetAsync("http://localhost:5202/SmcdXmasTree/5000");
+// hint: 10000 kills the server with stack overflow
+
+
+Console.WriteLine(res.StatusCode);
